33e00655dbfe74ba378d5b15f651b40c01667cf3
Brian W Reaves <junk@brianreaves.com>
2025-12-03 15:36:02 +1000

    Update import type for SpeechService in factory.ts

--- lib/speech/factory.ts
+++ lib/speech/factory.ts
@@ -1,6 +1,6 @@
 import { DeepgramService } from "./deepgram-service";
 import { MicrophoneService } from "./microphone-service";
-import type { SpeechServiceOptions, SpeechServiceKey } from "./types";
+import type { SpeechService, SpeechServiceKey } from "./types";
 
 export function getSpeechService(service: SpeechServiceKey, options: SpeechServiceOptions) {
   if (service === "deepgram") {
